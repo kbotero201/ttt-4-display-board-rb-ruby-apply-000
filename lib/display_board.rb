ebdd867f3521b85c2b16d6d8f036492ec417794f
Kevin Botero <kbotero201@gmail.com>
2020-06-28 00:29:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,7 +9,7 @@
 #end
 
 def display_board(person) 
-  puts "#{person[0]}|#{person[1]}|#{person[2]}"
+  puts " #{person[0]} | #{person[1]} | #{person[2]} "
   puts "-----------"
   puts " #{person[3]}  | #{person[4]}  |  #{person[5]} "
   puts "-----------"
